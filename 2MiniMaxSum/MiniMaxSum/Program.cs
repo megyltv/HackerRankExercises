d66cc1c0a6a0a9ec3e84da96218e9af5ef9b06a5
Megan Toinga <megyltv@hotmail.com>
2022-03-23 16:13:13 -0400

    MT - Adds basic structure for program

---
+++ 2MiniMaxSum/MiniMaxSum/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MiniMaxSum
+{
+    public class Program
+    {
+        static void Main(string[] args){
+            Console.WriteLine("Hello");
+        }
+    }
+}
---
+++ 2MiniMaxSum/MiniMaxSumTest/MiniMaxSumTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MiniMaxSumTest
+{
+    public class MiniMaxSumTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
