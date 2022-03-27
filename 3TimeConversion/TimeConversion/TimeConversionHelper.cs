635a2176136a56c10fde09e8a9141c08feab05bb
Megan Toinga <megyltv@hotmail.com>
2022-03-27 18:54:37 -0400

    MT - Refactors code to use DateTime parser

--- 3TimeConversion/TimeConversion/TimeConversionHelper.cs
+++ 3TimeConversion/TimeConversion/TimeConversionHelper.cs
@@ -5,18 +5,8 @@ namespace TimeConversion
     public class TimeConversionHelper
     {
         public string Convert(string date) {
-            var period = date.Substring(date.Length-2);
-            Int16.TryParse(date.Substring(0,2), out var hourInt);
-
-            if (period == "AM" && hourInt == 12) {
-                return $"00:{date.Substring(3, date.Length - 5)}";
-            }
-
-            if (period == "PM" && hourInt != 12) {
-                hourInt += 12;
-                return $"{hourInt}:{date.Substring(3, date.Length - 5)}";
-            }
-            return date.Substring(0, date.Length - 2);
+            DateTime.TryParse(date, out var time);
+            return time.ToString("HH:mm:ss");
         }
     }
 }
