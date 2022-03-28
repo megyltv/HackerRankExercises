2e542405b6df3ddef6f2626b8885c6c966c1d22e
Megan Toinga <megyltv@hotmail.com>
2022-03-27 20:32:52 -0400

    MT - Adds record to min and max depending on the score

--- 4BreakingTheRecords/BreakingTheRecords/BreakingTheRecord.cs
+++ 4BreakingTheRecords/BreakingTheRecords/BreakingTheRecord.cs
@@ -1,8 +1,27 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
 
 namespace BreakingTheRecords
 {
-    public class BreakingTheRecord
+    public static class BreakingTheRecord
     {
+        public static List<int> CalculateRecord(List<int> scores) {
+            var maxScoreRecord = 0;
+            var minScoreRecord = 0;
+            var maxScore = scores.First();
+            var minScore = scores.First();
+
+            foreach(var score in scores) {
+                if (maxScore < score) { 
+                    maxScore = score;
+                    maxScoreRecord++; 
+                } else if (minScore > score) {
+                    minScore = score;
+                    minScoreRecord++;
+                }
+            }
+            return new List<int> { maxScoreRecord, minScoreRecord };
+        }
     }
 }
--- 4BreakingTheRecords/BreakingTheRecordsTests/BreakingTheRecordTests.cs
+++ 4BreakingTheRecords/BreakingTheRecordsTests/BreakingTheRecordTests.cs
@@ -1,3 +1,7 @@
+using BreakingTheRecords;
+using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
 using System;
 using Xunit;
 
@@ -6,9 +10,53 @@ namespace BreakingTheRecordsTests
     public class BreakingTheRecordTests
     {
         [Fact]
-        public void Test1()
+        public void CalculateRecord_ShouldRecordsBeZero_WhenThereIsOnlyOneScore()
         {
+            var scores = new List<int> { 10 };
 
+            var actualResults = BreakingTheRecord.CalculateRecord(scores);
+
+            actualResults.Should().Equal(new List<int> {0,0});
+        }
+
+        [Fact]
+        public void CalculateRecord_ShouldRecordsStayAsZero_WhenNoScoreHasBeenBroken()
+        {
+            var scores = new List<int> { 10, 10 };
+
+            var actualResults = BreakingTheRecord.CalculateRecord(scores);
+
+            actualResults.Should().Equal(new List<int> {0,0});
+        }
+
+        [Fact]
+        public void CalculateRecord_ShouldAddsRecordForMostPoints_WhenTheScoreIsBroken()
+        {
+            var scores = new List<int> { 10, 20 };
+
+            var actualResults = BreakingTheRecord.CalculateRecord(scores);
+
+            actualResults.Should().Equal(new List<int> {1,0});
+        }
+
+        [Fact]
+        public void CalculateRecord_ShouldAddsRecordForLessPoints_WhenTheScoreIsBroken()
+        {
+            var scores = new List<int> { 10, 5 };
+
+            var actualResults = BreakingTheRecord.CalculateRecord(scores);
+
+            actualResults.Should().Equal(new List<int> {0, 1});
+        }
+
+        [Fact]
+        public void CalculateRecord_ShouldAddsRecord_WhenSendingMultipleScores()
+        {
+            var scores = new List<int> { 10, 5, 20, 20, 4, 5, 2, 25, 1 };
+
+            var actualResults = BreakingTheRecord.CalculateRecord(scores);
+
+            actualResults.Should().Equal(new List<int> {2, 4});
         }
     }
 }
